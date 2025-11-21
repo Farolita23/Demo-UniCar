edcd81eb4858467caaf35cdb23568bba9530739a
Adam Gavira <gaviraadam@gmail.com>
2025-11-21 08:45:02 +0100

    Primer commit, crear base estructural del proyecto

---
+++ BackEnd/UniCar/src/main/java/com/Daw/UniCar/UniCarApplication.java
@@ -0,0 +1,13 @@
+package com.Daw.UniCar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniCarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniCarApplication.class, args);
+	}
+
+}
---
+++ BackEnd/UniCar/src/test/java/com/Daw/UniCar/UniCarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Daw.UniCar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniCarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
