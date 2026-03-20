8993dcaa1539ab29448b6b63327567fd961a757c
javfalrea <javierfalconreal2003@gmail.com>
2026-03-20 11:06:36 +0100

    Resuelto problematica CORS

--- BackEnd-Unicar/target/generated-sources/annotations/com/daw/controller/dto/mapper/RatingMapperImpl.java
+++ BackEnd-Unicar/target/generated-sources/annotations/com/daw/controller/dto/mapper/RatingMapperImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2026-03-08T11:39:41+0100",
+    date = "2026-03-20T10:38:42+0100",
     comments = "version: 1.6.3, compiler: Eclipse JDT (IDE) 3.43.0.v20250819-1513, environment: Java 21.0.8 (Eclipse Adoptium)"
 )
 @Component
